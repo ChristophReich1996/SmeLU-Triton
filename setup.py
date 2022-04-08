ffcf018771ddeaa8794b3b7da4538b56d161df8c
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2022-04-08 17:25:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="smelu",
     version="0.1",
-    url="https://github.com/ChristophReich1996/SmeLU",
+    url="https://github.com/ChristophReich1996/SmeLU-Triton",
     license="MIT License",
     author="Christoph Reich",
     author_email="ChristophReich@gmx.net",
-    description="PyTorch SmeLU",
+    description="Triton/PyTorch SmeLU",
     packages=["smelu",],
     install_requires=["torch>=1.0.0"],
 )
